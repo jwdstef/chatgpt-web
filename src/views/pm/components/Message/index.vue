994f113df2ad62d08156b587cb31013d7546acc3
李亚超 <410000368@qq.com>
2023-04-04 14:00:59 +0800

    屏蔽掉了头像显示

--- src/views/pm/components/Message/index.vue
+++ src/views/pm/components/Message/index.vue
@@ -87,12 +87,12 @@ function handleRegenerate() {
     class="flex w-full mb-6 overflow-hidden"
     :class="[{ 'flex-row-reverse': inversion }]"
   >
-    <div
-      class="flex items-center justify-center flex-shrink-0 h-8 overflow-hidden rounded-full basis-8"
-      :class="[inversion ? 'ml-2' : 'mr-2']"
-    >
-      <AvatarComponent :image="inversion" />
-    </div>
+<!--    <div-->
+<!--      class="flex items-center justify-center flex-shrink-0 h-8 overflow-hidden rounded-full basis-8"-->
+<!--      :class="[inversion ? 'ml-2' : 'mr-2']"-->
+<!--    >-->
+<!--      <AvatarComponent :image="inversion" />-->
+<!--    </div>-->
     <div class="overflow-hidden text-sm " :class="[inversion ? 'items-end' : 'items-start']">
       <p class="text-xs text-[#b4bbc4]" :class="[inversion ? 'text-right' : 'text-left']">
         {{ dateTime }}
