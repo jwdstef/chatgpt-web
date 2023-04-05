eb2fcc8028cee12b299402e44b3374d1f5c137dd
jwd <jwd1986@163.com>
2023-04-05 20:58:27 +0800

    feat(lang): 修改错误页面

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -48,17 +48,16 @@ onMounted(() => {
           奥创·10X智能机器人基于开源OpenAI GPT构建，仅仅用于各位好友内使用，让大家先人一步体验最先进的AI职能。如有疑问联系QQ：25463204。
         </p>
       </div>
-      <!-- <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
+      <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI">
         {{ $t("setting.balance") }}：{{ config?.balance ?? '-' }}
-        <span class="text-xs text-neutral-400">({{ $t('setting.monthlyUsage') }})</span>
       </p>
       <p v-if="!isChatGPTAPI">
         {{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}
       </p>
       <p>{{ $t("setting.timeout") }}：{{ config?.timeoutMs ?? '-' }}</p>
       <p>{{ $t("setting.socks") }}：{{ config?.socksProxy ?? '-' }}</p>
-      <p>{{ $t("setting.httpsProxy") }}：{{ config?.httpsProxy ?? '-' }}</p> -->
+      <p>{{ $t("setting.httpsProxy") }}：{{ config?.httpsProxy ?? '-' }}</p>
     </div>
   </NSpin>
 </template>
